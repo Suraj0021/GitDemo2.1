3517788e2f6853d61674a1c154fcac820a85295c
suraj <surajmargale777@gmail.com>
2023-07-24 16:19:01 +0530

    First Commit

---
+++ app/src/main/java/com/suraj/gitdemo2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.suraj.gitdemo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println("this is master branch")
+
+
+    }
+}
